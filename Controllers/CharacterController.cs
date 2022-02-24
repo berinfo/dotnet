c12047435feb3299f63237fcdb6f9ef869c67fa6
berinfo <berinfo@maestralsolutions.com>
2022-02-24 09:48:07 +0100

    added routes for get all and single character

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using dotnet.Models;
+using System.Collections.Generic;
+
+namespace dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Name = "Sam"}
+        };
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+        [HttpGet]
+
+        public ActionResult<Character> GetSignle()
+        {
+            return Ok(characters[0]);
+        }
+
+    }
+}
