14c990d89248582b642e3e30926357b97b4e8b04
berinfo <berinfo@maestralsolutions.com>
2022-02-25 16:35:44 +0100

    relation between users and characters in db

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace dotnet.Models
 {
     public class User
@@ -6,5 +8,6 @@ namespace dotnet.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public List<Character> Characters { get; set; }
     }
 }
