d6b3528145b46649e3da07d9187ca21c6f6b0110
berinfo <berinfo@maestralsolutions.com>
2022-02-28 09:38:30 +0100

    authentication user registration with user exists auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using dotnet.Data;
+using dotnet.Dtos.User;
+using dotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
