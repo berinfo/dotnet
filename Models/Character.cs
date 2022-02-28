1c5e76d9f36d916bbce25b684310e1f78dd522a6
berinfo <berinfo@maestralsolutions.com>
2022-02-28 16:37:45 +0100

    many to many relationship

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace dotnet.Models
 {
     public class Character
@@ -11,6 +13,7 @@ namespace dotnet.Models
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public User User { get; set; }
         public Weapon Weapon { get; set; }
+        public List<Skill> Skills { get; set; }
 
     }
 }
